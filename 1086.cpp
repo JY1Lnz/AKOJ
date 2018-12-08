7b7a8a87551b45374052e22102a0aa84443efaf6
晋毓麟 <2522471070@qq.com>
2018-12-08 09:19:52 +0800

    commit 1086

---
+++ 1086.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+int data[210][210];
+
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    for (int i = 0;i<n;i++)
+        for(int j = 0;j<m;j++)
+            cin>>data[i][j];
+    int maxline = 0;
+    for (int i = 0;i<n;i++)
+    {
+        for (int j = 0;j<m;j++)
+        {
+            int tmp = data[i][j];
+            for (int l = 1;)
+        }
+    }
+
+    return 0;
+}
