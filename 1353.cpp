7f56baa8bd4fd215b95cfa9d853384b4ac6696e3
晋毓麟 <2522471070@qq.com>
2018-12-16 23:56:14 +0800

    AC1353

--- 1353.cpp
+++ 1353.cpp
@@ -3,28 +3,29 @@ using namespace std;
 
 const int maxn  = 20001+10;
 int dp[maxn];
+int data[20];
+int vis[20];
 
 int main()
 {
-    int n,money = 0;
-    cin>>n;
-    int T[n+1],C[n+1];
-    for (int i = 1;i<=n;i++)
-        cin>>T[i]>>C[i];
-    cin>>money;
-    for (int i = 1;i<=n;i++)
+    int n,m;
+    while(cin>>n)
     {
-        for (int k = 1;k<=C[i];k++)
-            for (int j = money;j<=T[i];j--)
-            {
-                dp[j] = max(dp[j],dp[j-T[i]]+T[i]);
-                if (money == dp[j])
-                {
-
-                }
-            }
+        for (int i = 1; i<=n; i++)
+            cin>>data[i]>>vis[i];
+        cin>>m;
+        dp[0] = 0;
+        for (int i = 1; i<=m; i++)
+            dp[i] = 99999;
+        for (int i = 1; i<=n; i++)
+            //for (int j = 1; j<=vis[i]; j++)
+                for (int k = m; k>=data[i]; k--)
+                    dp[k] = min(dp[k],dp[k-data[i]]+1);
+        if (dp[m] != 99999)
+            cout<<dp[m]<<endl;
+        else
+            cout<<-1<<endl;
     }
-
 
     return 0;
 }
