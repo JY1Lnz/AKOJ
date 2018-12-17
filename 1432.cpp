686f0719156390a2a2f5e8e439b662bafc5d5ab2
晋毓麟 <2522471070@qq.com>
2018-12-17 17:28:51 +0800

    AC1432 优先队列拓扑排序

--- 1432.cpp
+++ 1432.cpp
@@ -1,9 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int maxn = 10000+10;
+/*
+优先队列实现拓扑排序
+vector储存每个点的下一个点
+优先队列实现最大值优先
+当一个点的入度为0时
+此点可以提出来存入优先队列
+题解链接：https://blog.csdn.net/fjinhao/article/details/52662554
+*/
+vector<int> G[maxn];
+int du[maxn],n,m,topo[maxn];
 
-int finda(vector<int> data,int n)
+bool toposort()
 {
-    vector<int>::iterator it = data.begin();
+
+    for (int i = 1;i<=n;i++)
+        for (int j = 0;j<G[i].size();j++)
+            du[G[i][j]]++;
+    int tot = 0;
+
+    priority_queue<int> q;
+    for (int i = 1;i<=n;i++)
+        if (!du[i])
+            q.push(i);
+    while(!q.empty())
+    {
+        int x = q.top();
+        q.pop();
+        topo[tot++] = x;
+        for (int j = 0;j<G[x].size();j++)
+        {
+            int t = G[x][j];
+            du[t]--;
+            if (!du[t])
+                q.push(t);
+        }
+    }
+    if (tot == n)
+        return true;
+    else
+        return false;
 
 }
 
@@ -13,24 +50,24 @@ int main()
     cin>>t;
     while(t--)
     {
-        vector<int> data;
-        int n,m;
+        int l,r;
         cin>>n>>m;
-        int book[n+1] = {0};
-        int a,b;
-        while(m--)
+        memset(du,0,sizeof(du));
+        for (int i = 0;i<=n;i++)
+            G[i].clear();
+
+        for (int i = 0;i<m;i++)
         {
-            cin>>a>>b;
-            if (data[a] == 0&&data[b] == 0)
-            {
-                data[a] = 1;
-                data[b] = 1;
-                data.push_back(a);
-                data.push_back(b);
-            }
-
-
+            cin>>l>>r;
+            G[l].push_back(r);
         }
+
+        if (toposort())
+            for (int i = 0;i<n;i++)
+                cout<<topo[i]<<' ';
+        else
+            cout<<-1;
+        cout<<endl;
 
     }
 
